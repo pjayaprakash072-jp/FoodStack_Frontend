58b1b5bb40735884459150ec0d31f0b09e89cf98
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-08-20 21:02:37 +0530

    Added the EmpthState component

--- src/components/Common/EmptyState.jsx
+++ src/components/Common/EmptyState.jsx
@@ -1,7 +1,13 @@
 
-const EmptyState = () => {
+const EmptyState = ({title = "Nothing here yet",
+                    text = "create your first record to get started"
+}) => {
   return (
-    <div>EmptyState</div>
+    <div className="empty">
+        <div className="empty-icon">+</div>
+        <h3>{title}</h3>
+        <p>{text}</p>
+    </div>
   )
 }
 
