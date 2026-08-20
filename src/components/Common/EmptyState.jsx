03488aeba479a7dc31c7862a519f6731cec908fb
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-08-20 21:15:23 +0530

    Modal is added

--- src/components/Common/EmptyState.jsx
+++ src/components/Common/EmptyState.jsx
@@ -1,13 +1,15 @@
 
 const EmptyState = ({title = "Nothing here yet",
                     text = "create your first record to get started"
-                    ,
+                    ,action,
+
 }) => {
   return (
     <div className="empty">
         <div className="empty-icon">+</div>
         <h3>{title}</h3>
         <p>{text}</p>
+        {action}
     </div>
   )
 }
--- src/components/Common/Modal.jsx
+++ src/components/Common/Modal.jsx
@@ -1,7 +1,20 @@
 
-const Modal = () => {
-  return (
-    <div>Modal</div>
+
+import {X} from 'lucide-react'
+const Modal = ({open,title,onClose,children}) => {
+    if(!open) return null;
+    return (
+    <div className="modal-backdrop" onMouseDown={onClose}>
+        <div className="modal" onMouseDown={(e)=> e.stopPropagation()}>
+            <div className="modal-header">
+                <h3>{title}</h3>
+                <button className="icon-button" onClick={onClose}>
+                    <X/>
+                </button>
+            </div>
+            {children}
+        </div>
+    </div>
   )
 }
 
